44045415cfa2e2353d1a0b759b909b8330de8943
Rafael Cruz <rafaelcruz.net81@gmail.com>
2023-12-16 11:12:59 -0300

    test: criacao de testes de unidade de usuário

---
+++ SpotifyLike.Tests/Domain/UsuarioTest.cs
@@ -0,0 +1,96 @@
+﻿using SpotifyLike.Domain.Conta.Agreggates;
+using SpotifyLike.Domain.Streaming.Aggregates;
+using SpotifyLike.Domain.Transacao.Agreggates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLike.Tests.Domain
+{
+    public class UsuarioTest
+    {
+        [Fact]
+        public void DeveCriarUsuarioComSucesso()
+        {
+            Plano plano = new Plano()
+            {
+                Descricao = "Lorem ipsum",
+                Id = Guid.NewGuid(),
+                Nome = "Plano Dummy",
+                Valor = 19.90M
+            };
+
+            Cartao cartao = new Cartao()
+            {
+                Id = Guid.NewGuid(),
+                Ativo = true,
+                Limite = 1000M,
+                Numero = "6465465466",
+            };
+
+            string nome = "Dummy Usuario";
+            string email = "teste@teste.com";
+            string senha = "123456";
+
+            //Act
+            Usuario usuario = new Usuario();
+            usuario.CriarConta(nome, email, senha, DateTime.Now, plano, cartao);
+
+            //Assert
+            Assert.NotNull(usuario.Email);
+            Assert.NotNull(usuario.Nome);
+            Assert.True(usuario.Email == email);
+            Assert.True(usuario.Nome == nome);
+            Assert.True(usuario.Senha != senha);
+
+            Assert.True(usuario.Assinaturas.Count > 0);
+            Assert.Same(usuario.Assinaturas[0].Plano, plano);
+
+            Assert.True(usuario.Cartoes.Count > 0);
+            Assert.Same(usuario.Cartoes[0], cartao);
+
+            Assert.True(usuario.Playlists.Count > 0);
+            Assert.True(usuario.Playlists[0].Nome == "Favoritas");
+            Assert.False(usuario.Playlists[0].Publica);
+        }
+
+        [Fact]
+        public void NaoDeveCriarUsuarioComCartaoSemLimite()
+        {
+            Plano plano = new Plano()
+            {
+                Descricao = "Lorem ipsum",
+                Id = Guid.NewGuid(),
+                Nome = "Plano Dummy",
+                Valor = 19.90M
+            };
+
+            Cartao cartao = new Cartao()
+            {
+                Id = Guid.NewGuid(),
+                Ativo = true,
+                Limite = 10M,
+                Numero = "6465465466",
+            };
+
+            string nome = "Dummy Usuario";
+            string email = "teste@teste.com";
+            string senha = "123456";
+
+            //Act
+            Assert.Throws<Exception>(() =>
+            {
+                Usuario usuario = new Usuario();
+                usuario.CriarConta(nome, email, senha, DateTime.Now, plano, cartao);
+            });
+            
+        }
+
+
+
+
+
+    }
+}
